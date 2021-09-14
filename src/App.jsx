606ca146531e9971fe84ae5dd6d087142cf52da1
Robin <rp.ullstorp92@gmail.com>
2021-09-14 16:06:23 +0200

    commit #5
    Fixat getData i music komponenten

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,10 @@ function App() {
   const [inputText, setInputText] = useState()
   let textInput = React.createRef();
 
-  function t() {
+  function searchFunction() {
     const value = textInput.current.value;
     setInputText({ value })
   }
-
 
   return (
     <Router>
@@ -26,17 +25,21 @@ function App() {
           <h1>The <br></br><span>Music</span><br></br> Player</h1>
         </div>
 
-        <div className="search">
-          <input type="text" ref={textInput} placeholder="Search for music/artists or albums"></input>
-          <button onClick={t}>search</button>
-        </div>
         <nav>
           <Link to="/"></Link>
         </nav>
       </div>
+
+      <div>
+        <div className="search">
+            <input type="text" ref={textInput}  placeholder="Search for music/artists or albums"></input>
+            <button onClick={searchFunction}>search</button>
+        </div>
+      </div>
+
       <Switch>
         <Route path="/">
-          <Music inputText={inputText} />
+          <Music inputText={inputText}/>
         </Route>
       </Switch>
     </Router>
--- src/components/Music.jsx
+++ src/components/Music.jsx
@@ -1,22 +1,31 @@
-import React, {useEffect} from 'react'
+import React, { useEffect, useState } from 'react'
 
-function Music(props) {   
-    console.log({props})
-
-    useEffect(async () => {
+function Music(props) {
+    const [songName, setSongName] = useState([])
+    console.log(props.inputText)
     
-        let result = await fetch(" https://yt-music-api.herokuapp.com/api/yt/artists/metallica")
-        let data = await result.json()
-        console.log(data)
-        },[])
+    // we will use async/await to fetch this data
+    async function getData() {
+        if (props.inputText != null){
+            console.log(props.inputText.value)
+            let result = await fetch(" https://yt-music-api.herokuapp.com/api/yt/artists/"+ props.inputText.value )
+            const data = await result.json();
+            const { content } = data
+
+            // store the data into our books variable
+            setSongName(content);
+            console.log(songName)
+        }
+    }
 
     return (
         <div>
-            <ul>
-                <li>
-                    <p>Artist {props.content}</p>
-                </li>
-            </ul>
+            <button onClick={getData}>Press me</button>
+            <div className="display">
+                {songName.map((item, index) => (
+                    <li key={index}><p>{item.name}</p></li>
+                ))}
+            </div>
         </div>
     )
 }
