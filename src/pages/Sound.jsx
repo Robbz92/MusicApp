b99bd8235c2bfe4b5b308a82f9cea6a17c6822bb
Robin <rp.ullstorp92@gmail.com>
2021-09-17 12:04:19 +0200

    commit#14
    lagt in musik id i spelListan

--- src/pages/Sound.jsx
+++ src/pages/Sound.jsx
@@ -1,26 +1,36 @@
 import React, { useEffect, useState } from 'react'
 
 function Sound(props) {
-    // hämtar enbart på låg
+    /**
+     * Denna props.match.params.id kan antingen vara song.videoId eller playList.browseId
+     * song.videoId är en låt och playList.browseId är en spellista.
+    */
     const { id } = props.match.params
     console.log(props)
 
-    // const type = id.length > 20 ? "playlist" : "song";
+    /** 
+     * Ternary : en låt (videoId) har ca 12 i längd. browseId har mer än 30 längd.
+     *  Om längden är mindre än 20 är det en låt annars är det en playlista.
+    */
 
-    // const [playlist, setPlayList] = useState([id]); // Denna ska vara en lista med strängar
+    const type = id.length > 20 ? "playlist" : "song";
+    let playList = []
+    //const [playlist, setPlayList] = useState([id]); // Denna ska vara en lista med strängar
     // const [playlistIndex, setPlaylistIndex] = useState(0);
     // const [ready, setReady] = useState(false);
 
-    // async function getPlaylist() {
-    //     let herokuappAPI = "https://yt-music-api.herokuapp.com/api/yt/playlist/"
-    //     let result = await fetch(herokuappAPI + id)
-    //     const data = await result.json();
-    //     const { content } = data
+    async function getPlaylist() {
+        let herokuappAPI = "https://yt-music-api.herokuapp.com/api/yt/playlist/"
+        let result = await fetch(herokuappAPI + id)
+        const data = await result.json();
+        const { content } = data
 
-    //     // om det är en play list sparar vi allt i useState.
-    //     const playList = content.map((videoId) => videoId.videoId)
-    //     return playList;
-    // } 
+        // om det är en play list sparar vi allt i useState.
+        // const playList = content.map((videoId) => videoId.videoId)
+        // return playList;
+        playList.push(content.map((videoId) => videoId.videoId))
+
+    } 
 
     let player;
 
@@ -42,22 +52,35 @@ function Sound(props) {
         if (event.data != YT.PlayerState.PLAYING) return
     }
 
+    /* När jag klickar på knapparna play/stop -> next
+    *  så körs denna methoden, validerar vilen av funktionerna som ska köras.
+    */
     const handleActions = (action) => {
         if (!player) return
+        if (type === "playlist") {
+            getPlaylist()
+            loadSong()
+        }
+
         if (action === "play") return player.playVideo()
         if (action === "pause") return player.pauseVideo()
     }
 
     // Om vi är på index 0, ladda index 1
-    const loadSong = (id) => {
+    const loadSong = () => {
         if (!player) return
-        player.loadVideoById(id)
+        for(let i = 0; i <= playList.length; i++){
+            console.log(playList)
+        }
+        
     } 
     
+    // kör useEffect direkt vid inladdning.
     useEffect(() => {
         onYouTubeIframeAPIReady()
     }, [])
 
+     // Jag vill kunna dela en låt på en länk
     const linkSoung = () => {
 
         const copyText = "http://localhost:3000"+ props.location.pathname
@@ -68,7 +91,6 @@ function Sound(props) {
         alert("Copied the text: " + copyText);
     }
 
-   // Jag vill kunna dela en låt på en länk
     return (
         <div className="soud-player-buttun-container">
             <button className="soud-player-button" onClick={() => handleActions("play")}>Play</button>
